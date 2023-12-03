89939e10cf7fb383c9cb2112a768dea33cb735dc
dozy-owl <97381427+dozy-owl@users.noreply.github.com>
2023-12-03 22:56:24 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,233 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ЕдаExpress',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Оплата заказа'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(
+              "Оплата заказа",
+              style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black,
+                  decoration: TextDecoration.none
+              )
+          ),
+          backgroundColor: Colors.blueAccent,
+      ),
+      body: Center(
+        child: ListView(
+          padding: EdgeInsets.all(32),
+          children: [
+            buildCardsList(),
+            buildPaymentAmountText(),
+            buildCardsDataInput(),
+            buildButtonPay(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildCardsList() {
+    return SizedBox(
+      height: 55.5,
+      child: Image.asset("assets/images/allCards.png"),
+      /*Row(
+        children: [
+          Expanded(child: Image.asset("assets/images/visa.jpg")),
+          Expanded(child: Image.asset("assets/images/mastercard.jpg")),
+          //https://vsememy.ru/image/wp-content/uploads/2023/03/1636161719_19-papik-pro-p-visa-logotip-foto-20.png"),
+          //https://tx-shop.ru/image/catalog/masterkard.jpg"),
+          Expanded(child: Image.asset("assets/images/world.jpg"))
+          //https://www.zaks.ru/f/2022/April/Mir_Logo.jpg"),
+        ],*/
+      );
+  }
+
+  Widget buildPaymentAmountText(){
+    final paymentAmount = 0;
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 8),
+          Text(
+              "Сумма оплаты:",
+              style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.black,
+                  decoration: TextDecoration.none
+              )
+          ),
+          const SizedBox(height: 4),
+          Text(
+              '$paymentAmount руб',
+              style: TextStyle(
+                  fontSize: 17,
+                  color: Colors.black,
+                  decoration: TextDecoration.none
+              )
+          )
+        ]
+    );
+  }
+
+  Widget buildCardsDataInput(){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const SizedBox(height: 12),
+        Text(
+          "Номер карты",
+          style: TextStyle(
+              fontSize: 17,
+              color: Colors.black,
+              decoration: TextDecoration.none
+
+          )
+        ),
+        const SizedBox(height: 8),
+        const SizedBox(
+          height: 40.0,
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              contentPadding: EdgeInsets.only(bottom: 1.0, left: 10.0),
+              //hintText: 'Enter a search term',
+            ),
+          ),
+        ),
+        const SizedBox(height: 12),
+        Text(
+            "Имя на карте",
+            style: TextStyle(
+                fontSize: 17,
+                color: Colors.black,
+                decoration: TextDecoration.none
+            )
+        ),
+        const SizedBox(height: 8),
+        const SizedBox(
+          height: 40.0,
+          child: TextField(
+            decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            contentPadding: EdgeInsets.only(bottom: 1.0, left: 10.0),
+            //hintText: 'Enter a search term',
+            ),
+          ),
+        ),
+        const SizedBox(height: 12),
+        Row(
+          children: [
+            Expanded(
+              child: Text(
+                  "Срок действия",
+                  style: TextStyle(
+                      fontSize: 17,
+                      color: Colors.black,
+                      decoration: TextDecoration.none
+                  )
+              ),
+            ),
+            Expanded(
+                child: Text(
+                    "CVC/CVV",
+                    style: TextStyle(
+                        fontSize: 17,
+                        color: Colors.black,
+                        decoration: TextDecoration.none
+                    )
+                ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 8),
+        const SizedBox(
+          height: 40.0,
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                //flex: 3,
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: "ММ / ГГ",
+                    contentPadding: EdgeInsets.only(bottom: 1.0, left: 10.0),
+                  ),
+                ),
+              ),
+              Expanded(
+                //flex: 4,
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    contentPadding: EdgeInsets.only(bottom: 1.0, left: 10.0),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 24.0),
+      ],
+    );
+  }
+
+  void onPressed(){}
+
+  Widget buildButtonPay(){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+          OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                shape: RoundedRectangleBorder(),
+                backgroundColor: Colors.blueAccent,
+              ),
+              onPressed: onPressed,
+              child: Text(
+                  "Оплатить",
+                  style: TextStyle(
+                      fontSize: 17,
+                      color: Colors.black,
+                      decoration: TextDecoration.none
+                  )
+              )
+          ),
+      ],
+    );
+
+  }
+}
